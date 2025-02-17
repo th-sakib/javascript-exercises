a1d9d7165d31f6e70c7909fbe5c4a69096fff775
th-sakib <mdthamidulhasansakib@gmail.com>
2025-02-17 22:08:20 +0600

    first two problem solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+let string = "";
+const repeatString = (keyword, repeat) => {
+  if (repeat < 0) {
+    string = "ERROR";
+  } else if (repeat === 0) {
+    string = "";
+  }
 
+  for (let i = 1; i <= repeat; i++) {
+    string = string + keyword;
+  }
+  return string;
 };
+
+// repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
